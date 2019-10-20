964c56e41c29be4a88f06f324d828dd9ac1db0b9
NO_ob <15161159+NO-ob@users.noreply.github.com>
2019-10-21 00:00:43 +0100

    Optimize Script

--- CBZ Minimizer.py
+++ CBZ Minimizer.py
@@ -3,10 +3,9 @@ import sys, os
 res = input("Resize Percentage: ")
 for x in sys.argv[1:]:
 	os.system("mkdir tmp")
-	os.system("unzip \"" + x +"\" -d tmp")
-		
-	for filename in os.listdir("tmp"):
-		os.system("mogrify -verbose -resize "+res+ "% tmp/\"" + filename + "\" ")
-		
+	os.system("unzip \"" + x +"\" -d tmp")	
+	os.system("mogrify -verbose -resize "+res+ "% tmp/*")	
 	os.system("zip -r \"" + x.split(".")[0]+"\"_min.cbz tmp/")
-	os.system("rm -rf tmp")
+	os.system("rm tmp/*")
+
+os.system("rm -rf tmp/")
